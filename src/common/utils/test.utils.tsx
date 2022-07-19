ecbeb332ae821aa04ad44dd47f546d3812f1712a
Chiman Seyedhashemi <ch.seyedhashemi@gmail.com>
2022-07-19 19:34:25 +0430

    Update test utils

--- src/common/utils/test.utils.tsx
+++ src/common/utils/test.utils.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { render as rtlRender } from '@testing-library/react';
 import { createMemoryHistory } from 'history';
 import { Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { mainReducer } from '../../redux/store';
 
 function renderWithRouter(ui: React.ReactElement, { route = '/', history = createMemoryHistory({ initialEntries: ['/'] }) } = {}) {
   return {
@@ -17,8 +20,26 @@ function renderWithRouter(ui: React.ReactElement, { route = '/', history = creat
   };
 }
 
+function renderWithRedux(ui: React.ReactElement, { initialState = {}, store = createStore(mainReducer), ...renderOptions } = {}) {
+  return rtlRender(ui, {
+    wrapper: ({ children }) => <Provider store={store}>{children}</Provider>,
+    ...renderOptions,
+  });
+}
+
+function renderWithRouterAndRedux(ui: React.ReactElement, { route = '/', history = createMemoryHistory({ initialEntries: ['/'] }) } = {}) {
+  return {
+    ...renderWithRedux(
+      <Router location={route} navigator={history}>
+        {ui}
+      </Router>
+    ),
+    history,
+  };
+}
+
 // re-export everything
 export * from '@testing-library/react';
 
 // override render method
-export { renderWithRouter };
+export { renderWithRouter, renderWithRedux, renderWithRouterAndRedux };
