4092f87a38b211b06c7400e4d7fa012ec907cc9b
Chiman Seyedhashemi <ch.seyedhashemi@gmail.com>
2022-07-19 08:57:49 +0430

    Trahslation file

--- src/common/interfaces.ts
+++ src/common/interfaces.ts
@@ -21,3 +21,7 @@ export interface ProcessedVideo {
   author: string;
   categories: string[];
 }
+
+export interface StringObject {
+   [key: string]: string;
+}
---
+++ src/common/translation.ts
@@ -0,0 +1,7 @@
+import { StringObject } from "./interfaces";
+
+export const translation: StringObject = {
+    videos: "Videos",
+    home: "Home",
+    aboutUs: "About Us",
+    faq:"FAQ"}
