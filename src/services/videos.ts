90e12057ce7df3ab96b8f26dfd1674fd6854e6a8
Chiman Seyedhashemi <ch.seyedhashemi@gmail.com>
2022-07-19 19:37:34 +0430

    Get videos service

--- src/services/videos.ts
+++ src/services/videos.ts
@@ -1,12 +1,29 @@
 import { getCategories } from './categories';
 import { getAuthors } from './authors';
-import { ProcessedVideo } from '../common/interfaces';
+import { ProcessedVideo } from '../common/model/video.model';
+
 
 export const getVideos = (): Promise<ProcessedVideo[]> => {
   return Promise
     .all([getCategories(),getAuthors()])
     .then(([categories, authors]) => {
-    // TODO: implement
-    return [];
+      let videos: ProcessedVideo[] = []
+
+      authors.forEach(author => {
+        const authorVideos: Array<ProcessedVideo> = author.videos.map(video => ({
+          id: video.id,
+          name: video.name,
+          author: author.name,
+          releaseDate: video.releaseDate,
+          formats:video.formats,
+          categories: categories.filter(category => video.catIds.find(id => id === category.id)).map(category => category.name)
+          
+        } ))
+
+        videos = videos.concat(authorVideos)
+
+      })
+
+    return videos;
   });
 };
