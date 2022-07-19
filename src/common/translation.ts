fcbe2f861a82d22b151bf39a07724e1bd8e43aaa
Chiman Seyedhashemi <ch.seyedhashemi@gmail.com>
2022-07-19 19:34:03 +0430

    Add translation

--- src/common/translation.ts
+++ src/common/translation.ts
@@ -4,4 +4,14 @@ export const translation: StringObject = {
     videos: "Videos",
     home: "Home",
     aboutUs: "About Us",
-    faq:"FAQ"}
+    faq: "FAQ",
+    videoName: "Video Name",
+    author: "Author",
+    categories: "Categories",
+    highestQualityFormat: "Highest quality format",
+    releaseDate: "Release Date",
+    options: "Options",
+    edit: "Edit",
+    delete: "Delete",
+    addVideo:"Add Video"
+}
